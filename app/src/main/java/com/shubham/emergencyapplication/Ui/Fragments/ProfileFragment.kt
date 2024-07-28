12f86ff6f1044a24d6d42e467ff1ebdeb8e4b01a
Shubham Raut <rautshubham280@gmail.com>
2024-07-29 01:18:29 +0530

    logout dialog ui

--- app/src/main/java/com/shubham/emergencyapplication/Ui/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/shubham/emergencyapplication/Ui/Fragments/ProfileFragment.kt
@@ -1,60 +1,66 @@
 package com.shubham.emergencyapplication.Ui.Fragments
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.TEXT_ALIGNMENT_CENTER
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
 import com.shubham.emergencyapplication.R
+import com.shubham.emergencyapplication.Ui.Activities.LoginActivity
+import com.shubham.emergencyapplication.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+
+    }
+    fun showLogout() {
+        try {
+            val dialogToCreateAlert = Dialog(requireContext())
+            dialogToCreateAlert.setContentView(R.layout.logout_dialog)
+
+            dialogToCreateAlert.window!!.setBackgroundDrawableResource(R.drawable.round_corner_card_bg)
+            dialogToCreateAlert.window!!
+                .setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+
+            val confirm = dialogToCreateAlert.findViewById<MaterialButton>(R.id.logout)
+            val cancel = dialogToCreateAlert.findViewById<MaterialButton>(R.id.cancel)
+
+            confirm.setOnClickListener {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                dialogToCreateAlert.dismiss()
+
             }
+            cancel.setOnClickListener {
+                dialogToCreateAlert.dismiss()
+            }
+
+            dialogToCreateAlert.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
