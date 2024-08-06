d3f7cacf1f45b968966c8cec18f8fa6712f98613
Shubham Raut <rautshubham280@gmail.com>
2024-08-07 02:05:10 +0530

    spash screen implemented

--- app/src/main/java/com/shubham/emergencyapplication/Ui/Activities/SplashActivity.kt
+++ app/src/main/java/com/shubham/emergencyapplication/Ui/Activities/SplashActivity.kt
@@ -1,13 +1,20 @@
 package com.shubham.emergencyapplication.Ui.Activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.shubham.emergencyapplication.R
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +23,31 @@ class SplashActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+        MainScope().launch { 
+            delay(2000)
+            startApplication()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        auth = FirebaseAuth.getInstance()
+        MainScope().launch {
+            delay(2000)
+            startApplication()
+        }
+    }
+
+    private fun startApplication() {
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, DashboardActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
